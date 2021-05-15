641b77ee5f59abf8b7ae8a48cd43677f7bc7c64f
javiermelendrez1 <81501034+javiermelendrez1@users.noreply.github.com>
2021-05-14 17:37:22 -0700

    added more restuarants
    - more restaurant data
    - sort buttons are there but no logic yet

--- js/app.js
+++ js/app.js
@@ -25,7 +25,44 @@ const restaurants = [{
     parking: 'No',
     menuItemsHad: 'Joe Latti',
     rating: '10'
+},
+{
+    id: 3,
+    name: 'Morrison Atwater Village',
+    img: '/img/morrison1.jpeg',
+    location: 'Atwater Village',
+    parking: 'Yes',
+    menuItemsHad: 'Morrison Fillet Burger',
+    rating: '10'
+},
+{
+    id: 4,
+    name: 'Sauced BBQ & Spirits',
+    img: '/img/sauced1.jpeg',
+    location: 'Anaheim',
+    parking: 'Yes',
+    menuItemsHad: '3 meat Combo',
+    rating: '9'
+},
+{
+    id: 5,
+    name: 'Urth Cafe',
+    img: '/img/urth1.jpeg',
+    location: 'DTLA',
+    parking: 'Yes',
+    menuItemsHad: 'Croissant',
+    rating: '7'
+},
+{
+    id: 6,
+    name: 'Wurstküche',
+    img: '/img/wurst1.jpeg',
+    location: 'Arts District',
+    parking: 'No',
+    menuItemsHad: 'Duck and Bacon Sausage',
+    rating: '10'
 }
+
 ]
 
 
